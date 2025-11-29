5b05b470a230abfa7cde9ebea7a5840db05631b8
Leyli Hudayberdiyeva <leylidostluk@gmail.com>
2025-11-29 14:54:23 +0300

    category tab, slider

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const tabs = document.querySelectorAll(".tab_btn");
-const all_content = document.querySelectorAll(".tab_content");
+const all_content = document.querySelectorAll(".content");
+const left_btn_icn = document.querySelector(".left_icon_png");
+const right_btn_icn = document.querySelector(".right_icon_png");
+const category_cards = document.querySelector(".category_cards");
+const category_card = document.querySelectorAll(".card");
+
+right_btn_icn.addEventListener('click', () => {
+    category_cards.scrollLeft += category_card.offsetWidth + 10;
+})
+
+left_btn_icn.addEventListener('click', () => {
+    category_cards.scrollLeft -= category_card.offsetWidth + 10;
+})
 
 tabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
-        tabs.forEach(tab => { tab.classList.remove('active') })
+    tab.addEventListener('click', (e) => {
+        tabs.forEach(tab => { tab.classList.remove('active') });
         tab.classList.add('active');
 
         var line = document.querySelector('.tab_btn_line');
         line.style.width = e.target.offsetWidth + "px";
         line.style.left = e.target.offsetLeft + "px";
 
-        all_content.forEach(content => {
-            content.classList.remove('active')
-        });
+        all_content.forEach(content => { content.classList.remove('active') });
         all_content[index].classList.add('active');
     })
+
 })
