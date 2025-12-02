efb53d8ba3bbc96e99a390262e5f2c59de6052b3
Leyli Hudayberdiyeva <leylidostluk@gmail.com>
2025-12-02 18:54:22 +0300

    responsive(mobile)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tabs = document.querySelectorAll(".tab_btn");
+/*const tabs = document.querySelectorAll(".tab_btn");
 const all_content = document.querySelectorAll(".content");
 const left_btn_icn = document.querySelector(".left_icon_png");
 const right_btn_icn = document.querySelector(".right_icon_png");
@@ -26,4 +26,26 @@ tabs.forEach((tab, index) => {
         all_content[index].classList.add('active');
     })
 
-})
+})*/
+
+const openMenu = document.querySelector('.home_foll_cat');
+const hamburgerMenu = document.querySelector('.hamburger_menu');
+const closeMenu = document.querySelector('.close_menu');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+
+function show() {
+
+    hamburgerMenu.style.display = 'flex';
+    hamburgerMenu.style.top = '0';
+
+}
+
+
+function close() {
+
+    closeMenu.style.top = '-100vh';
+
+}
